8918862542f124be7545807411359532fedb969b
Goran Ehrsson <goran@technipelago.se>
2014-04-30 08:47:24 +0200

    Upgraded release plugin to 2.2.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,7 +19,7 @@ grails.project.dependency.resolution = {
     }
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.0.4") {
+              ":release:2.2.1") {
             export = false
         }
         test(":hibernate:$grailsVersion") {
